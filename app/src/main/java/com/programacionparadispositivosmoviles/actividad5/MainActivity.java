173b2e3255a4f04db026531617a9d30b77bb7f3e
José Fernández <fajjaf@gmail.com>
2019-08-30 16:15:43 -0300

    Primera modificacion

--- app/src/main/java/com/programacionparadispositivosmoviles/actividad5/MainActivity.java
+++ app/src/main/java/com/programacionparadispositivosmoviles/actividad5/MainActivity.java
@@ -2,13 +2,25 @@ package com.programacionparadispositivosmoviles.actividad5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CambioAvion ca;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        this.ca=new CambioAvion();
+        registerReceiver(this.ca,new IntentFilter("android.intent.action.AIRPLANE_MODE"));
+        super.onResume();    }
+        @Override
+            protected void onPause() {
+        unregisterReceiver(ca);
+        super.onPause();
     }
 }
